c03a5bd77c5740bf1ebe2fa28166f85f831fd5cc
kChulEe <kshin33@gmail.com>
2015-12-18 05:37:11 -0500

    Demo almost finisehd

--- index.php
+++ index.php
@@ -1,96 +1,87 @@
 <!DOCTYPE html>
 <html>
     <head>
-    
+		<!--this line is for ajax implementation, this is needed to call php inside js-->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     </head>
     
     <body>
-  
-        <!-- 1. The <iframe> (and video player) will replace this <div> tag. -->
-        <div id="player"></div>
-        
+        <!--placeholder for YouTube-->
+		<div id="player"></div>
+        <!--placeholder for checking frame progression-->
         <p id="demo"></p>
-
-        <script>
-          // 2. This code loads the IFrame Player API code asynchronously.
-          var tag = document.createElement('script');
-
-          tag.src = "https://www.youtube.com/iframe_api";
-          var firstScriptTag = document.getElementsByTagName('script')[0];
-          firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-          // 3. This function creates an <iframe> (and YouTube player)
-          //    after the API code downloads.
-          var player;
-          function onYouTubeIframeAPIReady() {
-            player = new YT.Player('player', {
-              height: '390',
-              width: '640',
-              videoId: 'Pj-TNdFBCq0',
-              events: {
-                'onReady': onPlayerReady,
-                'onStateChange': onPlayerStateChange
-              }
-            });
-          }
-
-          // 4. The API will call this function when the video player is ready.
-          function onPlayerReady(event) {
-            event.target.playVideo();
-          }
-
-          // 5. The API calls this function when the player's state changes.
-         var videoTime = 0;
-          function onPlayerStateChange(event) {
-            if (event.data == 2) {
-              videoTime = player.getCurrentTime();
-               document.getElementById("demo").innerHTML = videoTime;
-            }
-          }
         
+		<!--this is where Javascript for YouTube starts-->
+        <script>
+			var tag = document.createElement('script');
+			tag.src = "https://www.youtube.com/iframe_api";
+			var firstScriptTag = document.getElementsByTagName('script')[0];
+			firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+			var player;
+			function onYouTubeIframeAPIReady() {
+				player = new YT.Player('player', {
+					height: '390',
+					width: '640',
+					videoId: 'Pj-TNdFBCq0',
+					events: {
+						'onReady': onPlayerReady,
+						'onStateChange': onPlayerStateChange
+					}
+				});
+			}
+			//automatically plays video once it loads
+			function onPlayerReady(event) {
+				event.target.playVideo();
+			}
+			//variable made for indicating frames
+			var videoTime = 0;
+			//whenever the video is paused, following calls are made
+			function onPlayerStateChange(event) {
+				if (event.data == 2) {
+					videoTime = player.getCurrentTime();
+					//testing if frame progression works
+				    //document.getElementById("demo").innerHTML = videoTime;
+					
+					//start frame selection
+					if (videoTime > 15 && videoTime < 30) {
+						if (window.XMLHttpRequest) {
+							xmlhttp = new XMLHttpRequest();
+        				} else {
+							xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+						}
+						xmlhttp.onreadystatechange = function() {
+							if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+								document.getElementById("demo1").innerHTML = xmlhttp.responseText;
+							}	
+						};
+						//call php function with its path
+						xmlhttp.open("GET","phpcalls/democall.php?q=",true);
+						xmlhttp.send();
+					}
+					//start frame selection
+					if (videoTime > 40 && videoTime < 50) {
+						if (window.XMLHttpRequest) {
+							xmlhttp = new XMLHttpRequest();
+        				} else {
+							xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+						}
+						xmlhttp.onreadystatechange = function() {
+							if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+								document.getElementById("demo1").innerHTML = xmlhttp.responseText;
+							}	
+						};
+						//call php function with its path
+						xmlhttp.open("GET","phpcalls/democall1.php?q=",true);
+						xmlhttp.send();
+					//every other frame, show blank in the placeholder
+					}else{
+						document.getElementById("demo1").innerHTML = "";
+					}
+				}
+			}
         </script>
         
-        <div>
-          <ul>
-            <?php
-                include 'includes/sql_connect.php';
-
-                $query1 = "select * from pants";
-                if ($stmt = $con->prepare($query1)) {
-                    $stmt->execute();
-                    $stmt->bind_result($field1, $field2, $field3, $field4, $field5);
-                    if ($stmt->fetch() == "TRUE") {
-                        $image = $field5;
-                        echo '<img src="'.$image.'" width="100" height="100" />';
-                        printf("%s, %s, $%s \n", $field2, $field3, $field4);
-                        printf("\n");
-                    }
-                    $stmt->close();
-                }
-            ?>
-          </ul>
-          <ul>
-            <?php
-                $query2 = "select * from shirts";
-                if ($stmt = $con->prepare($query2)) {
-                    $stmt->execute();
-                    $stmt->bind_result($field1, $field2, $field3, $field4, $field5);
-                    if ($stmt->fetch() == "TRUE") {
-                        $image = $field5;
-                        echo '<img src="'.$image.'" width="100" height="100" />';
-                        printf("%s, %s, $%s \n", $field2, $field3, $field4);
-                    }
-                    $stmt->close();
-                }
-
-            ?>
-          </ul>
-        </div>
-
-
-        
-        
+		<!--placeholder for the query result-->
+		<p id="demo1"></p>  
     </body>
-
-
 </html>
---
+++ phpcalls/democall.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<style>
+			table {
+				width: 80%;
+				border-collapse: collapse;
+			}
+
+			table, td, th {
+				border: 1px solid black;
+				padding: 5px;
+				width: 500px;
+			}
+
+			th {text-align: middle;
+				witdth: 500px;
+			}
+		</style>
+	</head>	
+	
+	<body>
+		<?php
+			include '../includes/sql_connect.php';
+
+			 $query1 = "select * from pants where pants_id = 1";
+			 $result = mysqli_query($con,$query1);
+
+			 echo "<table>
+				<tr>
+				<th>Image</th>
+				<th>Brand</th>
+				<th>Name</th>
+				<th>Price</th>
+				</tr>";
+			  while($row = mysqli_fetch_array($result)) {
+				  echo "<tr>";
+				  echo "<td>" . '<img src="'.$row['pants_image'].'" width="100" height="100" />' . "</td>";
+				  echo "<td>" . $row['pants_brand'] . "</td>";
+				  echo "<td>" . $row['pants_name'] . "</td>";
+				  echo "<td>" . $row['pants_price'] . "</td>";
+				  echo "</tr>";
+			  }
+			  echo "</table>";
+		
+			 $query2 = "select * from shirts where shirt_id = 1";
+			 $result = mysqli_query($con,$query2);
+
+			 echo "<table>
+				<tr>
+				<th>Image</th>
+				<th>Brand</th>
+				<th>Name</th>
+				<th>Price</th>
+				</tr>";
+			  while($row = mysqli_fetch_array($result)) {
+				  echo "<tr>";
+				  echo "<td>" . '<img src="'.$row['shirt_image'].'" width="100" height="100" />' . "</td>";
+				  echo "<td>" . $row['shirt_brand'] . "</td>";
+				  echo "<td>" . $row['shirt_name'] . "</td>";
+				  echo "<td>" . $row['shirt_price'] . "</td>";
+				  echo "</tr>";
+			  }
+			  echo "</table>";
+		
+		mysqli_close($con);
+		?>
+	</body>
+</html>
